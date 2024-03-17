df587c356ed8b808992d528ef69a8e0c597d6e06
headache <h3adxach3@gmail.com>
2024-03-17 22:37:00 +0500

    lets go

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
@@ -12,6 +11,26 @@ def main_page():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/task1')
+def task1():
+    return render_template('task1.html')
+
+
+@app.route('/task2')
+def task2():
+    return render_template('task2.html')
+
+
+@app.route('/task3')
+def task3():
+    return render_template('task3.html')
+
+
+@app.route('/task4')
+def task4():
+    return render_template('task4.html')
 
 
 if __name__ == ('__main__'):
