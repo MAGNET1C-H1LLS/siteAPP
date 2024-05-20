1956e6fccc67ab0a90972d649802cb3e8ff3ecc8
root <root@cv3637521.novalocal>
2024-05-20 12:36:29 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = os.urandom(24)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'logi'
-context = ('./certificate.crt', './private.key')
+context = ('./../certificate.crt', './../private.key')
 
 
 class User(UserMixin):
